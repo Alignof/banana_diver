814583a3e7e89b84ddb235cc0622569715fe2ab9
Alingof <Alignof@outlook.com>
2022-03-08 21:16:22 +0900

    [add] add the module clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::{AppSettings, arg};
+
 fn main() {
-    println!("Hello, world!");
+    let app = clap::command!()
+        .arg(arg!(<inputfile> ... "dts file filename"))
+        .arg(arg!(-o <outputfile> ... "dtb file name").required(false))
+        .setting(AppSettings::DeriveDisplayOrder)
+        .get_matches();
+
+    dbg!(app);
 }
